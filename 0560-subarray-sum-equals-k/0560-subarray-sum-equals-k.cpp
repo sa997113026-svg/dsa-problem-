7338f82da93e01d49abee870e63e59c8ae3d89b0
sa997113026-svg <sa997113026@gmail.com>
2026-09-06 22:21:33 +0530

    Time: 2459 ms (16.66%), Space: 34.2 MB (99.41%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,32 +2,22 @@ class Solution {
 public:
     int subarraySum(vector<int>& arr, int k) {
         int n = arr.size();
-        int count = 0; // Added count variable
+        int count =0;
         
-        // 1. Build prefix sum array
-        vector<int> prefixSum(n, 0);
-        prefixSum[0] = arr[0];
-        for(int i = 1; i < n; i++) { // Started at i = 1 to prevent i-1 out of bounds
-            prefixSum[i] = prefixSum[i-1] + arr[i];
-        }
-        
-        unordered_map<int, int> m; 
-        
-        // 2. Count subarrays
-        for(int j = 0; j < n; j++) { // Started at j = 0
-            if(prefixSum[j] == k) {
-                count++;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum+=arr[j];
+                if(sum == k) count++;
+
+                
             }
-            
-            int val = prefixSum[j] - k; // Fixed typo and variable scope
-            if(m.find(val) != m.end()){
-                count += m[val];
-            }
-            
-            // Just increment the map, don't reset it to 0
-            m[prefixSum[j]]++;
         }
-        
         return count;
     }
 };
+
+
+
+        
+        
